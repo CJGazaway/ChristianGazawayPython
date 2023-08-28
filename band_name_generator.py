dcf96031f562c0fa127a80c7a50554416edc9815
CJGazaway <111094183+CJGazaway@users.noreply.github.com>
2023-08-28 00:39:10 -0400

    Add files via upload
    Need a new band name? Use my Band Name Generator! Coded in Python.

---
+++ band_name_generator.py
@@ -0,0 +1,10 @@
+print("Welcome to the Band Name Generator!")
+
+
+name = input("What is your name?\n")
+pet_name = input("What is your pets name?\n")
+color = input("What is your favorite color?\n")
+
+band_name = color + " " + pet_name + " " + name
+
+print(f"Your band name is: {band_name}")
